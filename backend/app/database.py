4c8f0c59893ada9e6dd1d7a1b4afb64962820664
NS <svidich.nadya@gmail.com>
2026-07-27 18:43:45 +0300

    something fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
 from typing import AsyncGenerator
+import ssl
 
 from app.config import settings
+
+# Создаём SSL-контекст (эквивалент sslmode=require)
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 # Создаём асинхронный движок SQLAlchemy
 engine = create_async_engine(
@@ -13,7 +19,7 @@ engine = create_async_engine(
     max_overflow=40,
     pool_pre_ping=True,
     pool_recycle=3600,
-    connect_args={"ssl": True}  # <--- добавляем SSL
+    connect_args={"ssl": ssl_context}  # <-- ПРАВИЛЬНЫЙ СПОСОБ
 )
 
 AsyncSessionLocal = async_sessionmaker(
